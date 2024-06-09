404a9df696602389b61da0c3bddfaf16951d4d1a
Mateo Castro <teocastro1405@gmail.com>
2024-06-09 18:37:37 -0500

    catalogo

--- logica1/src/main/java/com/example/Main.java
+++ logica1/src/main/java/com/example/Main.java
@@ -23,23 +23,25 @@ public class Main {
                     chevrolet(scanner);
                     mostrarMenu = false; // Después de elegir una marca, no mostramos el menú
                     break;
-                /*
-                 * case 2:
-                 * ford(scanner);
-                 * break;
-                 * case 3:
-                 * kia(scanner);
-                 * break;
-                 * case 4:
-                 * mazda(scanner);
-                 * break;
-                 * case 5:
-                 * renault(scanner);
-                 * break;
-                 * case 6:
-                 * toyota(scanner);
-                 * break;
-                 */
+                
+                case 2:
+                    ford(scanner);
+                    mostrarMenu = false;
+                    break;
+                case 3:
+                    kia(scanner);
+                    mostrarMenu = false;
+                    break;
+                case 4:
+                   mazda(scanner);
+                   break;
+                case 5:
+                  renault(scanner);
+                  break;
+                  case 6:
+                  toyota(scanner);
+                  break;
+                 
                 case 0:
                     System.out.println("Saliendo del programa...");
                     return;
@@ -74,13 +76,12 @@ public class Main {
             case 1:
                 onix(scanner);
                 break;
-        
              case 2:
-             aveo(scanner);
-             break;
+                aveo(scanner);
+                break;
              case 3:
-             sparkGT(scanner);
-             break;
+                sparkGT(scanner);
+                break;
             case 0:
                 break;
             default:
@@ -91,7 +92,6 @@ public class Main {
     private static void onix(Scanner scanner) {
         System.out.println("Informacion del vehiculo: ");
         System.out.println("Kilometros: 0" + ", " + "Precio: $15.000usd" + ", " + "Color: Blanco");
-
     }
     
     private static void aveo(Scanner scanner) {
@@ -105,4 +105,227 @@ public class Main {
       System.out.println("Kilometros: 45.000" +", "+ "Precio: $7.000usd" +", "+
       "Color: Azul");
     }
+
+    //Opción 2, Ford---------------------------------------------------------------------
+    private static void ford(Scanner scanner) {
+        System.out.println("1. Explorer");
+        System.out.println("2. Edge");
+        System.out.println("3. Raptor");
+        System.out.print("Seleccione un vehiculo: ");
+
+        int ford = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea
+
+        switch (ford) {
+            case 1:
+                explorer(scanner);
+                break;
+             case 2:
+                edge(scanner);
+                break;
+             case 3:
+                raptor(scanner);
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Opción no válida. Intente de nuevo.");
+        }
+    }
+
+    private static void explorer(Scanner scanner) {
+        System.out.println("Informacion del vehiculo: ");
+        System.out.println("Kilometros: 0" + ", " + "Precio: $20.000usd" + ", " + "Color: Negro");
+    }
+    
+    private static void edge(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 8.000" +", "+ "Precio: $15.000usd" +", "+
+      "Color: Beige");
+      }
+
+    private static void raptor(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 2.000" +", "+ "Precio: $30.000usd" +", "+
+      "Color: Azul");
+    }
+
+    //Opción 3, Kia---------------------------------------------------------------------
+    private static void kia(Scanner scanner) {
+        System.out.println("1. Picanto");
+        System.out.println("2. Sportage");
+        System.out.println("3. K3");
+        System.out.print("Seleccione un vehiculo: ");
+
+        int kia = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea
+
+        switch (kia) {
+            case 1:
+                picanto(scanner);
+                break;
+             case 2:
+                sportage(scanner);
+                break;
+             case 3:
+                k3(scanner);
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Opción no válida. Intente de nuevo.");
+        }
+    }
+
+    private static void picanto(Scanner scanner) {
+        System.out.println("Informacion del vehiculo: ");
+        System.out.println("Kilometros: 0" + ", " + "Precio: $20.000usd" + ", " + "Color: Blanco");
+    }
+    
+    private static void sportage(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 8.000" +", "+ "Precio: $15.000usd" +", "+
+      "Color: Gris");
+      }
+
+    private static void k3(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 2.000" +", "+ "Precio: $30.000usd" +", "+
+      "Color: Azul");
+    }
+
+    //Opción 4, Mazda ---------------------------------------------------------------------
+    private static void mazda(Scanner scanner) {
+        System.out.println("1. Mazda 2");
+        System.out.println("2. Mazda 3");
+        System.out.println("3. Mazda CX-30");
+        System.out.print("Seleccione un vehiculo: ");
+
+        int mazda = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea
+
+        switch (mazda) {
+            case 1:
+                mazda2(scanner);
+                break;
+             case 2:
+                mazda3(scanner);
+                break;
+             case 3:
+                mazdaCX30(scanner);
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Opción no válida. Intente de nuevo.");
+        }
+    }
+
+    private static void mazda2(Scanner scanner) {
+        System.out.println("Informacion del vehiculo: ");
+        System.out.println("Kilometros: 0" + ", " + "Precio: $20.000usd" + ", " + "Color: Blanco");
+    }
+    
+    private static void mazda3(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 8.000" +", "+ "Precio: $15.000usd" +", "+
+      "Color: Gris");
+      }
+
+    private static void mazdaCX30(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 2.000" +", "+ "Precio: $30.000usd" +", "+
+      "Color: Azul");
+    }
+
+     //Opción 5, Renault ---------------------------------------------------------------------
+     private static void renault(Scanner scanner) {
+        System.out.println("1. Logan");
+        System.out.println("2. Duster");
+        System.out.println("3. Sandero");
+        System.out.print("Seleccione un vehiculo: ");
+
+        int renault = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea
+
+        switch (renault) {
+            case 1:
+                logan(scanner);
+                break;
+             case 2:
+                duster(scanner);
+                break;
+             case 3:
+                sandero(scanner);
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Opción no válida. Intente de nuevo.");
+        }
+    }
+
+    private static void logan(Scanner scanner) {
+        System.out.println("Informacion del vehiculo: ");
+        System.out.println("Kilometros: 0" + ", " + "Precio: $20.000usd" + ", " + "Color: gris");
+    }
+    
+    private static void duster(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 8.000" +", "+ "Precio: $15.000usd" +", "+
+      "Color: blanco");
+      }
+
+    private static void sandero(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 2.000" +", "+ "Precio: $30.000usd" +", "+
+      "Color: negro");
+    }
+
+     //Opción 6, Toyota ---------------------------------------------------------------------
+     private static void toyota(Scanner scanner) {
+        System.out.println("1. Fortuner");
+        System.out.println("2. Corolla");
+        System.out.println("3. Four Runner");
+        System.out.print("Seleccione un vehiculo: ");
+
+        int toyota = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea
+
+        switch (toyota) {
+            case 1:
+                fortuner(scanner);
+                break;
+             case 2:
+                corolla(scanner);
+                break;
+             case 3:
+                fourrunner(scanner);
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Opción no válida. Intente de nuevo.");
+        }
+    }
+
+    private static void fortuner(Scanner scanner) {
+        System.out.println("Informacion del vehiculo: ");
+        System.out.println("Kilometros: 0" + ", " + "Precio: $20.000usd" + ", " + "Color: gris");
+    }
+    
+    private static void corolla(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 8.000" +", "+ "Precio: $15.000usd" +", "+
+      "Color: blanco");
+      }
+
+    private static void fourrunner(Scanner scanner) {
+      System.out.println("Informacion del vehiculo: ");
+      System.out.println("Kilometros: 2.000" +", "+ "Precio: $30.000usd" +", "+
+      "Color: negro");
+    }
+
+    //Informacion de dinero
+    
 }
